68a59c35de8e86d23b9490df52e1a596dc9d9e2d
alan tseng <alan@geovision.com.tw>
2013-11-06 11:53:13 +0800

    add demo to show the selected date information

--- CalendarControl/CalendarControlAppDelegate.h
+++ CalendarControl/CalendarControlAppDelegate.h
@@ -6,10 +6,14 @@
 
 #import <Cocoa/Cocoa.h>
 
+@class AFCalendarControl;
+
 @interface CalendarControlAppDelegate : NSObject <NSApplicationDelegate> {
     NSWindow *window;
 }
 
 @property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet AFCalendarControl* calendar;
+@property (assign) IBOutlet NSTextField* selectedDate;
 
 @end
--- CalendarControl/CalendarControlAppDelegate.m
+++ CalendarControl/CalendarControlAppDelegate.m
@@ -5,6 +5,7 @@
 //
 
 #import "CalendarControlAppDelegate.h"
+#import "AFCalendarControl.h"
 
 @implementation CalendarControlAppDelegate
 
@@ -13,6 +14,27 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    if (self.calendar)
+    {
+        [self.calendar setSelectAction:self action:@selector(syncSelectedDateInfo)];
+    }
+    
+    [self syncSelectedDateInfo];
+}
+
+- (void) syncSelectedDateInfo
+{
+    if (!self.calendar || !self.selectedDate)
+    {
+        return;
+    }
+    
+    NSDate* selectedDate = [self.calendar currentMonth];
+    NSTimeZone* timeZone = [NSTimeZone defaultTimeZone];
+    NSInteger seconds = [timeZone secondsFromGMTForDate:selectedDate];
+    NSDate* local = [NSDate dateWithTimeInterval:seconds sinceDate:selectedDate];
+    
+    [self.selectedDate setStringValue:[local description]];
 }
 
 @end
